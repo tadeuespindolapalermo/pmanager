35e815d5f7c16764382e4a47d199cec3dafa8e34
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-06 21:23:15 -0300

    Initial commit

---
+++ src/main/java/com/java360/pmanager/PManagerApplication.java
@@ -0,0 +1,13 @@
+package com.java360.pmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PManagerApplication.class, args);
+	}
+
+}
