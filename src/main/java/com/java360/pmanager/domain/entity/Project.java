2631625f12c88d1a0a0a4bef4e3a84f9382c215b
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-06 23:05:31 -0300

    Transformando a classe Project em uma entidade

--- src/main/java/com/java360/pmanager/domain/entity/Project.java
+++ src/main/java/com/java360/pmanager/domain/entity/Project.java
@@ -1,6 +1,7 @@
 package com.java360.pmanager.domain.entity;
 
 import com.java360.pmanager.domain.model.ProjectStatus;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,21 +9,35 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.UUID;
+
+@Entity
+@Table(name = "project")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = UUID)
+    @Column(name = "id", nullable = false, length = 36)
     private String id;
 
+    @Column(name = "name", nullable = false, length = 80)
     private String name;
 
+    @Column(name = "description", nullable = false, length = 150)
     private String description;
 
+    @Column(name = "initial_date", nullable = false)
     private LocalDate initialDate;
 
+    @Column(name = "final_date", nullable = false)
     private LocalDate finalDate;
 
+    @Column(name = "status", nullable = false)
+    @Enumerated(STRING)
     private ProjectStatus status;
 }
