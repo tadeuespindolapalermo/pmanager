06e56d20cafb07bf3468b04787294da6f57c4bda
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-06 22:52:51 -0300

    Usando Lombok para a classe Project

--- src/main/java/com/java360/pmanager/domain/entity/Project.java
+++ src/main/java/com/java360/pmanager/domain/entity/Project.java
@@ -1,9 +1,17 @@
 package com.java360.pmanager.domain.entity;
 
 import com.java360.pmanager.domain.model.ProjectStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
 
     private String id;
