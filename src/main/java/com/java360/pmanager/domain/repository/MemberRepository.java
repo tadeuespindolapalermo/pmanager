a255f11e8694be926b1d556543707a438876410d
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-07 21:35:17 -0300

    Criando o repositório de membros

---
+++ src/main/java/com/java360/pmanager/domain/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.java360.pmanager.domain.repository;
+
+import com.java360.pmanager.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+    Optional<Member> findByIdAndDeleted(String id, boolean deleted);
+
+    Optional<Member> findByEmailDeleted(String email, boolean deleted);
+}
