790937622a94279caa2897e44bfd5b863b914af8
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-08 21:45:54 -0300

    Adicionando os relacionamentos nos DTOs

--- src/main/java/com/java360/pmanager/infrastructure/dto/MemberDTO.java
+++ src/main/java/com/java360/pmanager/infrastructure/dto/MemberDTO.java
@@ -1,7 +1,13 @@
 package com.java360.pmanager.infrastructure.dto;
 
 import com.java360.pmanager.domain.entity.Member;
+import com.java360.pmanager.domain.entity.Project;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 public class MemberDTO {
@@ -9,13 +15,20 @@ public class MemberDTO {
     private final String secret;
     private final String name;
     private final String email;
+    private final Set<String> projectIds;
 
     public static MemberDTO create(Member member) {
         return new MemberDTO(
             member.getId(),
             member.getSecret(),
             member.getName(),
-            member.getEmail()
+            member.getEmail(),
+            Optional
+                .ofNullable(member.getProjects())
+                .orElse(List.of())
+                .stream()
+                .map(Project::getId)
+                .collect(Collectors.toSet())
         );
     }
 }
--- src/main/java/com/java360/pmanager/infrastructure/dto/ProjectDTO.java
+++ src/main/java/com/java360/pmanager/infrastructure/dto/ProjectDTO.java
@@ -1,10 +1,15 @@
 package com.java360.pmanager.infrastructure.dto;
 
+import com.java360.pmanager.domain.entity.Member;
 import com.java360.pmanager.domain.entity.Project;
 import com.java360.pmanager.domain.model.ProjectStatus;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 public class ProjectDTO {
@@ -14,6 +19,7 @@ public class ProjectDTO {
     private final LocalDate initialDate;
     private final LocalDate finalDate;
     private final ProjectStatus status;
+    private final Set<String> memberIds;
 
     public static ProjectDTO create(Project project) {
         return new ProjectDTO(
@@ -22,7 +28,13 @@ public class ProjectDTO {
             project.getDescription(),
             project.getInitialDate(),
             project.getFinalDate(),
-            project.getStatus()
+            project.getStatus(),
+            Optional
+                .ofNullable(project.getMembers())
+                .orElse(List.of())
+                .stream()
+                .map(Member::getId)
+                .collect(Collectors.toSet())
         );
     }
 }
