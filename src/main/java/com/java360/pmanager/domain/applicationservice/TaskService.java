e83aa365b3da014027723af9475cfe99770d0575
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-11 16:25:18 -0300

    Criando tarefas associadas a um projeto e membro

--- src/main/java/com/java360/pmanager/domain/applicationservice/TaskService.java
+++ src/main/java/com/java360/pmanager/domain/applicationservice/TaskService.java
@@ -1,5 +1,7 @@
 package com.java360.pmanager.domain.applicationservice;
 
+import com.java360.pmanager.domain.entity.Member;
+import com.java360.pmanager.domain.entity.Project;
 import com.java360.pmanager.domain.entity.Task;
 import com.java360.pmanager.domain.exception.InvalidTaskStatusException;
 import com.java360.pmanager.domain.exception.TaskNotFoundException;
@@ -11,21 +13,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import static com.java360.pmanager.domain.model.TaskStatus.PENDING;
+import static java.util.Objects.nonNull;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
 
+    private final MemberService memberService;
+    private final ProjectService projectService;
     private final TaskRepository taskRepository;
 
     @Transactional
     public Task createTask(SaveTaskDataDTO saveTaskData) {
+        Project project = null;
+        Member member = null;
+
+        if (nonNull(saveTaskData.getProjectId())) {
+            project = projectService.loadProject(saveTaskData.getProjectId());
+        }
+
+        if (nonNull(saveTaskData.getMemberId())) {
+            member = memberService.loadMemberById(saveTaskData.getMemberId());
+        }
+
         Task task = Task
             .builder()
             .title(saveTaskData.getTitle())
             .description(saveTaskData.getDescription())
             .numberOfDays(saveTaskData.getNumberOfDays())
             .status(PENDING)
+            .project(project)
+            .assignedMember(member)
             .build();
 
         taskRepository.save(task);
