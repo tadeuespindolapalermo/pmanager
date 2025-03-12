2fad880f189386ad88b89f97098dd96348800d21
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-11 23:19:53 -0300

    Finalizando a configuração do Spring Security

--- src/main/java/com/java360/pmanager/PManagerApplication.java
+++ src/main/java/com/java360/pmanager/PManagerApplication.java
@@ -3,9 +3,16 @@ package com.java360.pmanager;
 import com.java360.pmanager.infrastructure.config.AppConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = {
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class
+	}
+)
 @EnableConfigurationProperties(AppConfigProperties.class)
 public class PManagerApplication {
 
