4c5baf4682d54aaed127fc7dd87a91b57a4b0aa7
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2025-03-11 22:36:18 -0300

    Criando o serviço de autenticação

---
+++ src/main/java/com/java360/pmanager/infrastructure/security/AuthenticationService.java
@@ -0,0 +1,24 @@
+package com.java360.pmanager.infrastructure.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class AuthenticationService {
+
+    private final static String AUTH_TOKEN_HEADER_NAME = "x-api-key";
+
+    public Authentication getAuthentication(HttpServletRequest request) {
+        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+
+        if (!Objects.equals(apiKey, "thekey")) {
+            throw new BadCredentialsException("Invalid API Key: " + apiKey);
+        }
+
+        return new ApiKeyAuthenticationToken(apiKey);
+    }
+}
